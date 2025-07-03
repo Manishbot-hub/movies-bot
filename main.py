16370acec388e4aed96bf1959a7758d7baee9435
Manishbot-hub <yadavmanish7878523506@gmail.com>
2025-07-03 05:53:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ async def delete_last(user_id, context):
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await delete_last(update.effective_user.id, context)
-    text = "👋 Welcome to Movies World! Use /addmovie, /uploadbulk, or /movies to browse."
+    text = "👋 Welcome to Movies World! Use /search, or /movies to browse."
     msg = await update.message.reply_text(text)
     user_last_bot_message[update.effective_user.id] = msg.message_id
 
